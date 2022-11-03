11837b4753d4322dacde4e0153351f8e7e6bc9b9
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-11-03 02:13:14 +0100

    implement hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,88 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-  }
+const Calculator = () => {
+  const data = { total: 0, next: null, operation: null };
+  const [state, setState] = useState(data);
 
-  handleClick = (event) => {
+  const handleClick = (event) => {
     const buttonName = event.target.innerText;
-    const result = calculate(this.state, buttonName);
-    this.setState(result);
+    const result = calculate(state, buttonName);
+    setState(result);
   };
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator">
-        <div className="result-container">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="grid-container">
-          <button type="button" onClick={this.handleClick}>
-            AC
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            +/-
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            %
-          </button>
-          <button type="button" onClick={this.handleClick} className="operator">
-            &#247;
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            7
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            8
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            9
-          </button>
-          <button type="button" onClick={this.handleClick} className="operator">
-            x
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            4
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            5
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            6
-          </button>
-          <button type="button" onClick={this.handleClick} className="operator">
-            -
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            1
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            2
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            3
-          </button>
-          <button type="button" onClick={this.handleClick} className="operator">
-            +
-          </button>
-          <button type="button" onClick={this.handleClick} className="zero">
-            0
-          </button>
-          <button type="button" onClick={this.handleClick}>
-            .
-          </button>
-          <button type="button" onClick={this.handleClick} className="operator">
-            =
-          </button>
-        </div>
+  const { total, next, operation } = state;
+  return (
+    <div className="calculator">
+      <div className="result-container">
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
-}
+      <div className="grid-container">
+        <button type="button" onClick={handleClick}>
+          AC
+        </button>
+        <button type="button" onClick={handleClick}>
+          +/-
+        </button>
+        <button type="button" onClick={handleClick}>
+          %
+        </button>
+        <button type="button" onClick={handleClick} className="operator">
+          &#247;
+        </button>
+        <button type="button" onClick={handleClick}>
+          7
+        </button>
+        <button type="button" onClick={handleClick}>
+          8
+        </button>
+        <button type="button" onClick={handleClick}>
+          9
+        </button>
+        <button type="button" onClick={handleClick} className="operator">
+          x
+        </button>
+        <button type="button" onClick={handleClick}>
+          4
+        </button>
+        <button type="button" onClick={handleClick}>
+          5
+        </button>
+        <button type="button" onClick={handleClick}>
+          6
+        </button>
+        <button type="button" onClick={handleClick} className="operator">
+          -
+        </button>
+        <button type="button" onClick={handleClick}>
+          1
+        </button>
+        <button type="button" onClick={handleClick}>
+          2
+        </button>
+        <button type="button" onClick={handleClick}>
+          3
+        </button>
+        <button type="button" onClick={handleClick} className="operator">
+          +
+        </button>
+        <button type="button" onClick={handleClick} className="zero">
+          0
+        </button>
+        <button type="button" onClick={handleClick}>
+          .
+        </button>
+        <button type="button" onClick={handleClick} className="operator">
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default Calculator;
